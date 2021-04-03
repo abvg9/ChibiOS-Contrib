848d66f673887f0b307a98039d31f7351dcd3e10
abvg9 <deveavg@gmail.com>
2021-04-03 10:55:00 +0200

    Update License and authority.

--- os/hal/boards/NRF52-DWM1001/board.c
+++ os/hal/boards/NRF52-DWM1001/board.c
@@ -1,11 +1,11 @@
 /*
-    Copyright (C) 2016 Stéphane D'Alu
+    Copyright (C) 2021 UCM-237
 
-    Licensed under the Apache License, Version 2.0 (the "License");
+    Licensed under the GPL License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
 
-        http://www.apache.org/licenses/LICENSE-2.0
+        https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
